3887dff96ee09954a78a60a244ded394ee6fb144
foxvox <windev@nate.com>
2024-02-29 05:37:15 +0900

    AsyncioTcpEchoClient update

--- AsyncioTcpEchoServer.py
+++ AsyncioTcpEchoServer.py
@@ -2,10 +2,10 @@ import asyncio
 
 async def handle_echo(reader, writer):
     data = await reader.read(100)
-    message = data.decode()
+    msg = data.decode()
     addr = writer.get_extra_info('peername')
-    print("Received %r from %r" % (message, addr))
-    print("Send: %r" % message)
+    print("Received %r from %r" % (msg, addr))
+    print("Send: %r" % msg)
     writer.write(data)
     await writer.drain()
     print("Close the client socket")
